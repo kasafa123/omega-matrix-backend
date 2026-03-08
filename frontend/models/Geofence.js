0495fce5e18ab9bed4c8f0121f3eaaeda86880b6
kasafa123 <kasafaa123@gmail.com>
2026-03-08 23:19:41 +0700

    Final update: add flight radar and admin toggle

---
+++ frontend/models/Geofence.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const GeofenceSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  coords: {
+    lat: Number,
+    lng: Number
+  },
+  radius: Number, // dalam km
+  color: String,
+  alerts: {
+    type: Number,
+    default: 0
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Geofence', GeofenceSchema);
